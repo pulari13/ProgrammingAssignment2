776707aaf4802743d0a2378eb0506f0e9b6b3fcb
pulari13 <48246988+pulari13@users.noreply.github.com>
2019-04-12 09:26:35 +0530

    Update Cachematrix1.R

--- Cachematrix1.R
+++ Cachematrix1.R
@@ -1,10 +1,5 @@
 ## Caching the Inverse of a Matrix:
 
-## Below are a pair of functions that are used to create a special object that 
-## stores a matrix and caches its inverse.
-
-
-
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
